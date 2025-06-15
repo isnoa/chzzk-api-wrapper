b63fd178d8b6a37715b3191bcacc9719701a92d2
isnoa <isnoa3166@gmail.com>
2025-06-15 16:49:08 +0900

    feat: MongoDB URI 환경 변수를 MONGODB_URI로 변경하고, Swagger 관련 코드 및 의존성을 정리하여 문서화 개선

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@ const path = require("path");
 
 module.exports = {
   tokenStorage: process.env.TOKEN_STORAGE || "json", // 'json' 또는 'mongodb'
-  mongoUri: process.env.MONGO_URI || "",
+  mongoUri: process.env.MONGODB_URI || "",
   tokenJsonPath: path.resolve(__dirname, "token.json"),
 };
